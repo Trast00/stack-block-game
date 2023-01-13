671a8d583f450c1e22b5e0371b863041059c44ff
Allassane <dickoallassane1997@gmail.com>
2023-01-13 18:50:58 +0100

    Fix linter errors

--- src/components/Block.jsx
+++ src/components/Block.jsx
@@ -1,8 +1,5 @@
-import { useEffect } from "react";
 
 const Block = (props) => {
-  let isNew = true
-
   return (
     <div id={props.id} 
     className="block-wrapper" style={{width:props.size}}>
