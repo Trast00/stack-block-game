dd3022e8a3ab7bdfb6f9f1f80a66daa1a7fceb6e
Allassane <dickoallassane1997@gmail.com>
2023-01-14 18:09:58 +0100

    Update static cube design

--- src/components/Block.jsx
+++ src/components/Block.jsx
@@ -2,7 +2,8 @@
 const Block = (props) => {
   return (
     <div id={props.id} 
-    className="block-wrapper" style={{width:props.size, zIndex:props.id}}>
+    className={(props.id === "0") ? "block-wrapper block-static" : "block-wrapper" }
+    style={{width:props.size, zIndex:props.id}}>
       
         {props.name} {props.id}
       
