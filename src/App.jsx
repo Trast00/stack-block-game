31857660047da82772e372e42ca4803668d11ae0
Allassane <dickoallassane1997@gmail.com>
2023-03-11 22:33:09 +0100

    Fix all eslint errors

--- src/App.jsx
+++ src/App.jsx
@@ -3,17 +3,23 @@ import './App.css';
 import ListBlock from './components/ListBlock';
 
 const App = () => {
-  const [score, setScore] = useState(-1)
+  const [score, setScore] = useState(-1);
   return (
     <div className="flex-center App">
       <h1>Click Tic Game</h1>
-      <ListBlock finishGame={(score) => {setGameStarted("finished"); setScore(score)}}
-      updateScore={(score) => setScore(score)}
+      <ListBlock
+        updateScore={(score) => setScore(score)}
       />
-      <footer></footer>
-      {(score <= 0)? "": <p className='score-popup'>Score: {score}</p>}
+      <footer />
+      {(score <= 0) ? '' : (
+        <p className="score-popup">
+          Score:
+          {' '}
+          {score}
+        </p>
+      )}
     </div>
   );
-}
+};
 
 export default App;
--- src/components/Block.jsx
+++ src/components/Block.jsx
@@ -1,12 +1,21 @@
+import PropTypes from 'prop-types';
 
 const Block = (props) => {
+  const { id, size } = props;
   return (
-    <div id={props.id} 
-    className={(props.id === "0") ? "block-wrapper block-static" : "block-wrapper" }
-    style={{width:props.size, zIndex:props.id}}>
-      {(props.id === "0")? <p>Tape to Play</p>: ""}
+    <div
+      id={id}
+      className={(id === '0') ? 'block-wrapper block-static' : 'block-wrapper'}
+      style={{ width: size, zIndex: id }}
+    >
+      {(id === '0') ? <p>Tape to Play</p> : ''}
     </div>
-  )
-}
+  );
+};
 
-export default Block; 
+Block.propTypes = {
+  id: PropTypes.number.isRequired,
+  size: PropTypes.number.isRequired,
+};
+
+export default Block;
